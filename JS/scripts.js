feca0d039907802059a01d936622e625feeaea14
vizssouza <vini.nelso@gmail.com>
2025-11-18 17:25:21 -0300

    Adicionando Animações

--- JS/scripts.js
+++ JS/scripts.js
@@ -17,3 +17,25 @@ document.getElementById("formulario").addEventListener("submit", function(event)
     // Abre o WhatsApp em uma nova aba
     window.open(link, "_blank");
 });
+
+// ANIMAÇÕES GLOBAIS
+// Coletando o observador da página
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting === true) {
+            entry.target.classList.add('show')
+        }
+    })
+})
+
+// Selecionando os elementos com a classe ".hidden"
+const elements = document.querySelectorAll('.hidden')
+
+// Selecionando 1 arquivos por vez da classe ".hidden"
+elements.forEach((element) => myObserver.observe(element))
+
+// Selecionando os elementos com a classe ".hidden"
+const elements2 = document.querySelectorAll('.hidden2')
+
+// Selecionando 1 arquivos por vez da classe ".hidden"
+elements2.forEach((element) => myObserver.observe(element))
